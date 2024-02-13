885b2068d9644f858e9af824fa489f6ad96bb1dc
gsarwar1 <156928580+gsarwar1@users.noreply.github.com>
2024-02-13 08:14:21 -0500

    Update MarioKartPlayer.java

--- MarioKartPlayer.java
+++ MarioKartPlayer.java
@@ -1,1 +1,18 @@
+public class MarioKartPlayer {
+    private String playerName;
+    private String character;
+    private int speed;
 
+    // Constructor
+    public MarioKartPlayer(String playerName, String character, int startingSpeed) {
+        this.playerName = playerName;
+        this.character = character;
+        this.speed = startingSpeed;
+    }
+
+    // Boost method
+    public void boost() {
+        speed += 10; // Increase speed by 10
+    }
+
+    // Display player information
